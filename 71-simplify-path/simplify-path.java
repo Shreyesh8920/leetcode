01aecb2814e6ea70d67b6dd5c565f017a81048fb
Shreyesh Singh <shreyeshsingh9999@gmail.com>
2026-08-14 15:06:36 +0530

    Time: 4 ms (96.66%) | Memory: 44.8 MB (76.81%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,22 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
-        String [] arr = path.split("/");
         StringBuilder sb = new StringBuilder("/");
         Deque<String> st = new ArrayDeque<>();
-        for(String x : arr){
+        for(String x : path.split("/")){
             if(!st.isEmpty() && x.equals("..")){
                 st.pop();
                 continue;
-            }else if(x.equals(".") || x.length()==0 || x.equals("..")){continue;}
-            else{
+            }else if(x.equals(".") || x.length()==0 || x.equals("..")){
+                continue;
+            }else{
                 st.push(x);
             }
         }
         while(!st.isEmpty()){
-            sb = sb.append(st.removeLast());
+            sb.append(st.removeLast());
             if(!st.isEmpty()) sb = sb.append("/");
         }
-        // sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
 }
